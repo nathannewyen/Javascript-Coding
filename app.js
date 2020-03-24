879fca38ac6bf7dd71ff2594f2a4a5f69476d64f
Nathan Nguyen <nhan13574@gmail.com>
2020-03-24 01:17:35 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+// for (var i = 0; i <= 100; i++) {
+//     if (i % 10 === 0) {
+//         console.log('Dojo');
+//     } else if (i % 5 === 0) {
+//         console.log('Coding');
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// var arr = [[20, 30, 40, 50, 60, 70], 2, 4];
+
+// var myArr = [arr[0][0], arr[0][1], arr[0][arr[0].length - 1]];
+// console.log(myArr);
+
+// arr.pop(2);
+// arr[0].pop(4);
+// arr[0].push(70);
+
+// console.log(arr[0]);
+
+// function removeVals(arr) {
+//   var newarr = "";
+//   for (var i = 0; i <= arr.length; i++) {
+//     for (var j = 0; j <= arr[0].length; j++) {
+//       if ((arr[i][j] = arr[0][0])) {
+//         newarr = arr[i][j];
+//         console.log(arr[i][j]);
+//       }
+//     }
+//   }
+// }
+
+// newarr = removeVals(arr);
